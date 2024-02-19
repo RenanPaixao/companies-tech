2aad5c37ceb3d7456a69f1efc7e6c0171446c0d3
renan <renanlemos21@gmail.com>
2024-02-18 22:04:19 -0300

    feat: adapt home page to all screen sizes

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,10 @@
       <v-img src="@/assets/icons/logo.webp" />
     </v-avatar>
     <p class="content mt-8">
-      Bem-vindo ao Tecnlogias nas empresas! Aqui, você tem acesso a um banco de
-      stack usadas nas empresas brasileiras. Se você está buscando insights
-      sobre as tecnologias que impulsionam as corporações, você está no lugar
-      certo.
+      Bem-vindo ao <span>Tecnlogias nas empresas</span>! Aqui, você tem acesso a
+      um banco de stack usadas nas empresas brasileiras. Se você está buscando
+      insights sobre as tecnologias que impulsionam as corporações, você está no
+      lugar certo.
     </p>
     <p class="mt-4">
       Oferecemos uma plataforma para explorar e compartilhar as stacks
@@ -16,7 +16,7 @@
       específica atualmente.
     </p>
 
-    <v-btn class="mt-8" variant="elevated"> Buscar empresa </v-btn>
+    <v-btn class="mt-8"> Buscar empresa </v-btn>
   </v-container>
 </template>
 
@@ -26,12 +26,50 @@
 .logo {
   align-self: center;
 }
+
 button {
   background-color: var(--secondary);
   color: white;
+  font-size: 0.9rem;
+}
+
+span {
+  color: #f6ab51;
 }
 .container {
   display: flex;
   flex-direction: column;
+
+  button {
+    margin: 0 auto;
+    width: 75%;
+  }
+
+  @media (min-width: 600px) {
+    max-width: 80%;
+  }
+
+  @media (min-width: 800px) {
+    max-width: 75%;
+    font-size: 1.3rem;
+
+    button {
+      width: 50%;
+      height: 2.5rem;
+      font-size: 1rem;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    width: 50%;
+
+    button {
+      font-size: 1.1rem;
+    }
+  }
+
+  @media (min-width: 1440px) {
+    width: 50%;
+  }
 }
 </style>
